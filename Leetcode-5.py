fcac3809eccb8205dfb9745c71909c87ac0f0b18
Pon5522 <99264752+yypy22@users.noreply.github.com>
2023-02-02 20:54:07 +0000

    leetcode-5

---
+++ Leetcode-5.py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        #I could not solve it, so this is model answer from zhenyulin
+        #originally from easy space O(1) answer
+        N, l, r = len(s), 0, 1
+        
+        for i in range(N):
+            for a, b in (i, i), (i, i+1):
+                while 0 <= a and b < N and s[a] == s[b]: a, b = a-1, b+1
+                if b-a > r-l: l, r = a+1, b
+        
+        return s[l:r]
